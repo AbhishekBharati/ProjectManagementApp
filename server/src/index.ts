5a66e8b81afafa996313540a68c5787d7b98cbb8
AbhishekBharati <210305105048@paruluniversity.ac.in>
2025-01-19 00:02:49 +0530

    Configured the Ts file

---
+++ server/src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+// Route Imports
+import projectRoutes from "./routes/projectRoutes";
+import taskRoutes from "./routes/taskRoutes"
+
+// CONFIGURATIONS :-
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send("This is Home route");
+})
+app.use("/projects", projectRoutes);
+app.use("/tasks", taskRoutes);
+
+//SERVER
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server Running on port ${port}`)
+})

3494cacab4bf292038d17e8abae0a9980ace9791
AbhishekBharati <210305105048@paruluniversity.ac.in>
2025-01-20 15:36:00 +0530

    Completed ListView Tab

--- client/src/app/projects/ListView/index.tsx
+++ client/src/app/projects/ListView/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import { useGetTasksQuery } from '../../state/api';
+import { Task, useGetTasksQuery } from '../../state/api';
+import Header from '@/app/(components)/Header';
+import TaskCard from '@/app/(components)/TaskCard';
 
 type Props = {
   id: string,
@@ -12,7 +14,16 @@ const ListView = ({ id, setIsModalNewTaskOpen }: Props) => {
   if (isLoading) return <div>Loading...</div>
   if (error) return <div>An Error occurred while fetching tasks</div>
   return (
-    <div>ListView</div>
+    <div className='px-4 pb-8 xl:px-6'>
+      <div className='pt-5'>
+        <Header name='list' />
+      </div>
+      <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 lg:gap-6'>
+        {tasks?.map((task: Task) => (
+          <TaskCard key={task.id} task={task} />
+        ))}
+      </div>
+    </div>
   )
 }
 

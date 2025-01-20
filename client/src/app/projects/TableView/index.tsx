acf1e2eae81d48564c92d062ba370c8e63c16fd8
AbhishekBharati <210305105048@paruluniversity.ac.in>
2025-01-20 23:15:49 +0530

    Fixed the code that can cause error

--- client/src/app/projects/TableView/index.tsx
+++ client/src/app/projects/TableView/index.tsx
@@ -56,13 +56,13 @@ const columns: GridColDef[] = [
     field: "author",
     headerName: "Author",
     width: 150,
-    renderCell: (params) => params.value.username || "Unknown"
+    renderCell: (params) => params.value?.username || "Unknown"
   },
   {
     field: "assignee",
     headerName: "Assignee",
     width: 150,
-    renderCell: (params) => params.value.username || "Unassiggned"
+    renderCell: (params) => params.value?.username || "Unassiggned"
   },
 ]
 
@@ -76,7 +76,15 @@ const TableView = ({ id, setIsModalNewTaskOpen }: Props) => {
   return (
     <div className='h-[540px] w-fuull px-4 pb-8 xl:px-6'>
       <div className='pt-5'>
-        <Header name='Table' isSmallText />
+        <Header name='Table'
+          buttonComponent={
+            <button className='flex items-center rounded bg-blue-primary px-3 py-2 text-white hover:bg-blue-600'
+              onClick={() => setIsModalNewTaskOpen(true)}
+            >Add Task</button>
+          }
+          isSmallText
+
+        />
       </div>
       <DataGrid
         rows={tasks || []}
